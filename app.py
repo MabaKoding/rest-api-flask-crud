497f88cce870b253c8667bc0f26b9a1cc10f9b1e
MabaKoding <nuryasin.daudpoetra@gmail.com>
2022-08-30 18:00:51 +0700

    update delete and update

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # import library
+from ast import Delete
 from urllib import response
 from flask import Flask, request
 from flask_restful import Resource, Api
@@ -50,26 +51,30 @@ identitas = {}
 
 
 # membuat class Resource
-class ContohResource(Resource):
-    # method get and post
+class listResource(Resource):
+
     def get(self):
         # menampilkan data dari database
         query = ModelDatabase.query.all()
 
         # melakukan iterisasi pada modelDatabase
         output = [{
+            "id": data.id,
             "nama": data.nama,
             "umur": data.umur,
             "alamat": data.alamat
         } for data in query]
 
         response = {
-            "code" : 200,
-            "msg"  : "Data berhasil ditampilkan",
-            "data" : output
+            "code": 200,
+            "msg": "Data berhasil ditampilkan",
+            "data": output
         }
 
         return response, 200
+
+
+class addResource(Resource):
 
     def post(self):
         dataNama = request.form["nama"]
@@ -85,8 +90,64 @@ class ContohResource(Resource):
         return response, 200
 
 
+class updateResource(Resource):
+
+    def put(self, id):
+        # konsumsi id untuk query di model database
+        # pilih data yang ingin dirubah berdasarkan id yang dimasukkan
+        query = ModelDatabase.query.get(id)
+
+        # form untuk pengeditan data
+        editNama = request.form["nama"]
+        editUmur = request.form["umur"]
+        editAlamat = request.form["alamat"]
+
+        # merubah nilai yang ada pada field database
+        query.nama = editNama
+        query.umur = editUmur
+        query.alamat = editAlamat
+
+        db.session.commit()
+
+        response = {"code": 200, "msg": "Data berhasil dirubah"}
+
+        return response, 200
+
+
+class deleteResource(Resource):
+    # menghapus data berdasarkan id
+    def delete(self, id):
+        query = ModelDatabase.query.get(id)
+
+        # memanggil methode untuk delete data by id
+        db.session.delete(query)
+        db.session.commit()
+
+        response = {"code": 200, "msg": "Data berhasil dihapus"}
+
+        return response, 200
+
+class deleteAllResource(Resource):
+    # menghapus semua data
+    def delete(self):
+        query = ModelDatabase.query.all()
+
+        #looping
+        for data in query:
+            db.session.delete(data)
+            db.session.commit()
+
+        response = {"code": 200, "msg": "Data berhasil dihapus semua"}
+
+        return response, 200
+
+
 # setup resource
-api.add_resource(ContohResource, "/api", methods=["GET", "POST"])
+api.add_resource(listResource, "/api/list-data", methods=["GET"])
+api.add_resource(addResource, "/api/insert-data", methods=["POST"])
+api.add_resource(updateResource, "/api/<id>/update-data", methods=["PUT"])
+api.add_resource(deleteResource, "/api/<id>/delete-data", methods=["DELETE"])
+api.add_resource(deleteAllResource, "/api/delete-data", methods=["DELETE"])
 
 if __name__ == "__main__":
     app.run(debug=True, port=5005)
